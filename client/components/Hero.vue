711d5974541c1ffb9d3c6039f31a3283c17d932c
kingrayhan <rayhan095@gmail.com>
2020-10-16 01:28:37 +0600

    CategoryScope

---
+++ client/components/Hero.vue
@@ -0,0 +1,41 @@
+<template>
+  <section class="hero is-medium is-primary is-bold">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">{{ title }}</h1>
+        <h2 class="subtitle">{{ subTitle }}</h2>
+
+        <div class="mt-2">
+          <NuxtLink
+            class="tag mr-2"
+            v-for="subCategory in subCategories"
+            :key="subCategory.id"
+            :to="{
+              name: 'categories-slug',
+              params: { slug: subCategory.slug },
+            }"
+            >{{ subCategory.name }}</NuxtLink
+          >
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: 'Hero title',
+    },
+    subTitle: {
+      type: String,
+      default: 'Hero title description',
+    },
+    subCategories: {
+      type: Array,
+    },
+  },
+}
+</script>
--- client/pages/categories/_slug.vue
+++ client/pages/categories/_slug.vue
@@ -1,21 +1,36 @@
 <template>
   <div>
-    <h1>Heyyy</h1>
-    <pre>
-        {{ category }}
-    </pre>
+    <Hero
+      :title="category.name"
+      :subTitle="category.description"
+      :subCategories="category.children"
+    />
+
+    <div class="section">
+      <div class="container">
+        <div class="columns is-multiline">
+          <div
+            class="column is-3"
+            v-for="product in products"
+            :key="product.id"
+          >
+            <ProductCard :product="product" />
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   async asyncData({ params, $axios, error }) {
-    try {
-      const { data } = await $axios.$get(`categories/${params.slug}`)
-      return { category: data }
-    } catch (e) {
-      error({ statusCode: 404, message: 'Category not found' })
-    }
+    const { data: products } = await $axios.$get(
+      `products?category=${params.slug}`
+    )
+    const { data: category } = await $axios.$get(`categories/${params.slug}`)
+
+    return { products, category }
   },
 }
 </script>
